f0e9521a3301edc73430b1fba8f44aa8699ac01b
Geek_Tech <geecktech.och2@gmail.com>
2024-10-03 20:37:04 +0600

    lesson2

--- lessons/lesson1.py
+++ lessons/lesson1.py
@@ -8,11 +8,12 @@ def a(b):
     print(b)
 
 
-p=a(7)
-class Car:
+# p=a(7)
+class Car(object):
     name = 'MERS'
 
     def __init__(self,model,year):
+
         self.model=model
         self.year=year
     def sayname(self):
@@ -23,9 +24,14 @@ class Car:
 
 
 
+
 # обьект\экземпляр
 mers=Car('bmw',1999)
 mers2=Car('312',2010)
 
-print(len(mers))
-print(mers2)
+if __name__ == '__main__':
+        print(mers2)
+        print(mers2)
+        print(mers2)
+        print(mers2)
+        print(mers2)
